76085c18a4130d44605d49735958dd60cec82a66
Rituraj Ranjan <rituraj.ranjan.42@gmail.com>
2021-05-14 02:39:00 +0530

    changed homepage styles to styled component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import "./homepage.styles.scss";
-import Directory from "../../components/directory/directory.component";
+import React from 'react';
+import { HomePageContainer } from './homepage.styles';
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => {
 	return (
-		<div className="homepage">
+		<HomePageContainer>
 			<Directory />
-		</div>
+		</HomePageContainer>
 	);
 };
 
